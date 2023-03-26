acf97e791270d089805601c4bb221f6da0afe5bb
batikan <emiruar123@gmail.com>
2023-03-26 17:24:37 +0300

    commit-2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//dataFunction dosyasın da bulunan getData metodunu çağırıyoruz.
 import getData from './dataFunction.js'
 
 getData(1);
--- dataFunction.js
+++ dataFunction.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
 async function getData(id) {
+    //ilk önce verilen id ye göre o kullanıcıyı getiriyoruz
     const{data:user} =await (axios(`https://jsonplaceholder.typicode.com/users/${id}`));
+
+    //gelen userı yazdırıyoruz console ekranına
     console.log(user);
+    //verilen id değerine göre postu yazdırıyoruz.
     const {data:post} =await (axios(`https://jsonplaceholder.typicode.com/posts/${id}`));
+    //gelen post değerini console ekranona yazdırıyoruz
     console.log(post);
 }
 
+//Fonksiyonumuzu export ediyoruz dışarıya kullanıma açıyoruz.
 export default getData;
